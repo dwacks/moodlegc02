d516149d6d9f938488eb855019e40d74d3333785
jncys <jncys@dhcp-64-110.xlate.eduroam.ucl.ac.uk>
2015-11-19 17:46:40 +0000

    initaliazing folder

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle29';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = 'moodle';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '1',
+);
+
+$CFG->wwwroot   = 'http://localhost:8888/moodle29';
+// $CFG->wwwroot   = 'http://192.168.0.10:8888/moodle29';
+$CFG->dataroot  = '/Applications/MAMP/data/moodle29';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
---
+++ mod/workshop/lang/en/workshopextra.php
@@ -0,0 +1,4 @@
+<?php
+
+$string['workshopthreeSixty'] = 'Workshop 360';
+$string['workshopthreeSixty_help'] = 'Select between standard workshop and the enhanced workshop 360 which allows authors to comment on thier feedback recieved';
